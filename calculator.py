54c3a21e1d7b9d883698e3c23be03c322d49f022
Thando Mahego <110168419+ThandoMashego@users.noreply.github.com>
2025-01-05 17:04:55 +0200

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,27 @@
+operator = input("please enter an operator (+ _ / *)")
+num1 = float(input("please enter the first number ?"))
+num2 = float(input("please enter the second number ?"))
+
+
+if operator == "+" :
+    result = num1 +  num2 
+
+elif operator == "-" :
+    result = num1 - num2 
+
+elif operator == "/" :
+    result = num1 / num2 
+
+elif operator == "*" :
+    result = num1 * num2
+
+print(result)
+#     result = num1 * num2 
+# elif operator == "-" :
+#     result = num1 - num2 
+# elif operator == "-" :
+#     result = num1 - num2 
+
+
+
+
